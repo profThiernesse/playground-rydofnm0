5230c52b489f615215d6a0be3a8273a0775bb20f
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-27 08:16:50 +0200

    Update test_bissex.cpp

--- cpp-project/src/Exercises/test_bissex.cpp
+++ cpp-project/src/Exercises/test_bissex.cpp
@@ -14,15 +14,19 @@ int main()
 void test_bissex() {
     
     int i, test_val[3]={2004,2000,1000};
-    
+    int ok=1;
     for (i=0;i<3;i++) {
         if (bissex(test_val[i])) {
-            printf("TECHIO> success true\n");   
+            ok = ok && 1;
         }
         else {
+            ok = ok && 0;
             printf("TECHIO> success false\n");
             printf("TECHIO> message --channel 'Erreur la fonction ne passe pas le test (%d)'\n",test_val[i]);
         }
-    }
-    
+true
+    if(ok)
+        printf("TECHIO> success true\n");
+    else  
+        printf("TECHIO> success false\n");
 }
