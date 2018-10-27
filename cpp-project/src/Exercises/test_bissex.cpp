44f785679a32f59dd7cef4bd22700cc0af9fbb9a
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-10-27 07:57:47 +0200

    Update test_bissex.cpp

--- cpp-project/src/Exercises/test_bissex.cpp
+++ cpp-project/src/Exercises/test_bissex.cpp
@@ -1,10 +1,12 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include <stdio.h>
+#include <stdlib.h>
 #include "bissex.h"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( bissex(2000) == 1 );
-    REQUIRE( bissex(2004) == 1 );
-    REQUIRE( bissex(2003) == 0 );
-    REQUIRE( bissex(2100) == 0 );
+int main()
+{
+    printf("%d\n",bissex(2004));
+    printf("%d\n",bissex(2000));
+    printf("%d\n",bissex(1000));
+
+    return 0;
 }
